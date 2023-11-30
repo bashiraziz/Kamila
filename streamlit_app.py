2cc447837ac2f9bb60776b74f17940dd17481e80
Bashir Aziz <bashiraziz@users.noreply.github.com>
2023-11-30 08:52:02 +0000

    Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#from openai import OpenAI
+from openai import OpenAI
 st.title("Provided with love by of Lubna Kent")
 
 note= (''' All three pages will provided similar response \n 1. "Ask Kamila a Question" page makes a call to OpenAI API to get an answer to a question and provides the response received from OpenAI. \n 2. "Ask Kamila to Compute Percent Complete" page makes a call to OpenAI API and OpenAI uses a "custom in-house built" function sent with the call to compute the percent complete.\n3. "Compute Percent Complete" page does not make a call to OpenAI API and uses a custom in-house function to compute percent complete. \n''')
